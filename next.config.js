53e4c4402f8a5a2bdb910708cc4c002427ae86bc
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-26 05:29:47 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const imageminOptipng = require('imagemin-optipng');
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+       imageminOptipng()
+    },
+  ],
+
+  // your other plugins here
   // Target must be serverless
-  target: 'serverless'
-};
+  {
+    target: "serverless",
+  },
+]);
